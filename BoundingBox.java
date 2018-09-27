d55ca685b48edea1b89159c7ade7ca23849bd57b
Caleb Spradlin <calebspradlin@dhcp-87-251.ram.secure.int.wireless.unca.edu>
2018-09-27 10:22:48 -0400

    crated constructor

--- BoundingBox.java
+++ BoundingBox.java
@@ -1,7 +1,11 @@
-public class BoundingBox{
+public static class BoundingBox{
     private Point upLeft = null;
     private double width = 0;
     private double  height = 0;
-
+    public BoundingBox(Point upLeft, double width, double height){
+        this.upLeft = upLeft;
+        this.width = width;
+        this.height = height;
+    }
 
 }
