d835d73732b9fabb3359199063fc478be1114241
Caleb Spradlin <calebspradlin@dhcp-87-90.ram.secure.int.wireless.unca.edu>
2018-10-01 16:51:59 -0400

    bdb javadoc

--- BoundingBox.java
+++ BoundingBox.java
@@ -1,24 +1,52 @@
+/**
+ * @author Caleb Spradlin
+ * @version Oct 1 2018
+ * Project 2
+ * BoundingBox class takes three params, a Point and two doubles
+ * representing the width and height of the bounding box that 
+ * encompasses the figure
+ */
+
 public class BoundingBox{
     private final Point upLeft;
     private final double width;
     private final double  height;
-
+    /**
+     * @param upLeft
+     * @param width
+     * @param height
+     * Constructor for BoundingBox
+     */
     public BoundingBox(Point upLeft, double width, double height){
         this.upLeft = upLeft;
         this.width = width;
         this.height = height;
     }
-
+    /**
+     * @return upLeft
+     * Getter for Point variable
+     */
     public Point getUpleft(){
         return upLeft;
     }
+    /**
+     * @return width
+     * Getter for double Width
+     */
     public double getWidth(){
         return width;
     }
+    /**
+     * @return height
+     * Getter for double height
+     */
     public double getHeight(){
         return height;
     }
-
+    /**
+     * @return boolean
+     * method can be used to compare this object to another object
+     */
     @Override
     public boolean equals(Object obj){
         if (this == obj) return true;
@@ -30,7 +58,10 @@ public class BoundingBox{
         if(!Double.valueOf(height).equals(bdb.height)) return false;
         return true;
     }
-
+     /**
+     *@return Integer rep of a hashcode
+     *Assigns a hash code to the object
+     */
     @Override
     public int hashCode(){
         int hash = 13;
@@ -39,7 +70,10 @@ public class BoundingBox{
         hash = 19 * hash + Double.valueOf(height).hashCode();
         return hash;
     }
-
+    /**
+     * @return String
+     * Returns a string describing the objects variables
+     */
     @Override
     public String toString(){
         return upLeft.toString() + "Width:  " + width + " Height: " + height+ "\n";
